dc20a828936657f178888e3227325395c3ac5fe7
Muneeb-Babar <muneeb63ahmad63@gmail.com>
2025-02-23 12:38:30 +0500

    Start Python

---
+++ pyBasics.py
@@ -0,0 +1,53 @@
+print('Wellcome to Python')
+
+# Variable Declearation
+
+myName="Muneeb"
+myAge=25
+weight=55.5
+is_present=True
+
+print(myName)
+
+# Check Type
+
+print(type(myName))
+print(type(myAge))
+print(type(is_present))
+
+mainNumber=24
+addNumber=1
+
+mainNumber +=addNumber #we can perform all operations like +,-,*,/
+print(mainNumber)
+# Output: 25
+
+
+#Now understand the concept of typecate
+
+24 + int("1") #This is typecate
+
+# Math expressions: Unfamiliar operators
+
+7%3 #remainder operator
+2**3 #power
+
+# Math expressions: Eliminating ambiguity (PEMDAS)
+(1 + 3) * 4
+(2 * 4) * 4 + 2
+
+# Concatenating Text Strings Methods
+
+first_name="Muneeb"
+last_name="Babar"
+
+first_name+" "+last_name # METHIOD-1
+
+concat_name=f"{first_name} {last_name}" # METHOD-2
+
+"{0} {1}".format(first_name, last_name)  # METHOD-3
+
+print(concat_name)
+
+
+
